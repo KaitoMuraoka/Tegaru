53ae3fc99f3f2e47833a3f7b08e7f0c6cf8b98dd
KaitoMuraoka <70003919+KaitoMuraoka@users.noreply.github.com>
2026-06-25 13:19:31 +0900

    Initial Commit

---
+++ Tegaru/TegaruApp.swift
@@ -0,0 +1,17 @@
+//
+//  TegaruApp.swift
+//  Tegaru
+//
+//  Created by KaitoMuraoka on 2026/06/25.
+//
+
+import SwiftUI
+
+@main
+struct TegaruApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TegaruTests/TegaruTests.swift
@@ -0,0 +1,18 @@
+//
+//  TegaruTests.swift
+//  TegaruTests
+//
+//  Created by KaitoMuraoka on 2026/06/25.
+//
+
+import Testing
+
+struct TegaruTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
